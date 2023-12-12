30ee69b11e9e92de30a730c82bea8695b984d259
nemmi555 <144948843+nemmi555@users.noreply.github.com>
2023-12-12 20:50:17 +0100

    Add files via upload

--- CREATE_PRIMARY_ TABLE.sql
+++ CREATE_PRIMARY_ TABLE.sql
@@ -3,7 +3,8 @@ SELECT *
 FROM czechia_price cp ;
 
 SELECT *
-FROM czechia_payroll cp2 ;
+FROM czechia_payroll cp2
+WHERE value_type_code = 5958;
 
 SELECT *
 FROM czechia_payroll_industry_branch cpib ;
@@ -20,21 +21,28 @@ FROM economies e ;
 
 
 
-CREATE TABLE t_milan_nemecek_project_SQL_primary_final_test1
+CREATE TABLE t_milan_nemecek_project_SQL_primary_final
 SELECT
+	cpc.code AS food_category_code,
 	cpc.name AS food_category,
 	cp.value AS food_price,
-	cpib.name AS industry,
-	cp2.value AS average_wages,
-	cp2.payroll_year,
-	cr.name AS region_name,
 	cpc.price_value,
-	cpc.price_unit
+	cpc.price_unit,
+	DATE_FORMAT(cp.date_from, '%d.%m.%Y') AS price_measured_from,
+    DATE_FORMAT(cp.date_to, '%d.%m.%Y') AS price_measured_to,
+	cpib.code AS industry_code,
+    cpib.name AS industry,
+	cp2.value AS average_wages,
+	cp2.payroll_year
 FROM
 	czechia_price cp
 JOIN czechia_payroll cp2 
 	ON
 	YEAR(cp.date_from) = cp2.payroll_year
+	AND cp2.value_type_code = 5958
+	AND cp.region_code IS NULL
+	AND cp2.calculation_code = 200
+	AND cp2.payroll_quarter = 1
 LEFT JOIN czechia_price_category cpc 
 	ON
 	cpc.code = cp.category_code
@@ -44,9 +52,8 @@ LEFT JOIN czechia_payroll_industry_branch cpib
 LEFT JOIN czechia_region cr 
 	ON
 	cr.code = cp.region_code
-WHERE
-	cp2.value_type_code = 5958
 ;
 
 -- smazani tabulky
-DROP TABLE t_milan_nemecek_project_sql_primary_final_test1 
+DROP TABLE t_milan_nemecek_project_sql_primary_final 
+
